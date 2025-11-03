7eec8542206339e2fba1b87a19875493a59680fd
OneMoveBRB <sh_amir2014@mail.ru>
2025-11-03 12:13:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,15 +13,20 @@ int main() {
     ListInsert(list, 1, 30);
     ListInsert(list, 2, 40);
     ListInsert(list, 2, 50);
-    // ListInsert(list, 2, 60);
-    // ListInsert(list, 2, 70);
+    ListInsert(list, 2, 60);
+    ListInsert(list, 2, 70);
     // ListInsert(list, 7, 60);
 
     // ListInsertAfter(&list, 6, 555);
     // ListInsert(&list, 7, 444);
     // ListDeleteAt(&list, 6);
-    ListPop(list);
-    // ListErase(list, 3);
+    // ListPop(list);
+    ListErase(list, 0);
+    // ListErase(list, 4);
+
+    // ListGroupNodes(list);
+    ListLinearization(list);
+    ListErase(list, 0);
 
     TextDumpList(list);
 
