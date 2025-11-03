b0a63e7d9330eae7543950b79120382dbf4a67ba
OneMoveBRB <sh_amir2014@mail.ru>
2025-11-03 12:09:25 +0300

    Update list.h

--- list.h
+++ list.h
@@ -7,6 +7,7 @@
 
 const int FIRST_SIZE    = 8;
 const int UNINITIALIZED = 0xBAD;
+const int NONE_CAPACITY = 0;
 const int FakeElemIdx   = 0;
 
 typedef int type;
@@ -16,6 +17,10 @@ typedef struct List_t List_t;
 enum ListErr_t {
     LIST_OK,
     LIST_OVERFLOW,
+    LIST_WRONG_CAPACITY,
+    LIST_ALLOCATION_FAILED,
+    LIST_GROUP_FAILED,
+    LIST_LINEARIZATION_FAILED,
     LIST_INSERT_BEFORE_UNINITIALIZED,
     LIST_INSERT_AFTER_UNINITIALIZED,
     LIST_INDEX_OUT_OF_RANGE,
@@ -24,7 +29,8 @@ enum ListErr_t {
 };
 
 ListErr_t ListInit(List_t** list);
-ListErr_t ListUpdate(List_t* list);
+ListErr_t ListReallocUp(List_t* list);
+ListErr_t ListReallocDown(List_t* list, size_t capacity = NONE_CAPACITY);
 ListErr_t ListDestroy(List_t** list);
 
 type ListGet(List_t* list, size_t position);
@@ -41,5 +47,8 @@ ListErr_t ListPushBack(List_t* list, type value);
 ListErr_t ListInsert(List_t* list, size_t position, type value);
 ListErr_t ListPop(List_t* list);
 ListErr_t ListErase(List_t* list, size_t position);
+
+ListErr_t ListLinearization(List_t* list);
+ListErr_t ListGroupNodes(List_t* list);
 
 #endif // LIST_H
